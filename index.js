459a0a22f11a475e889eaef83f8c8c5b35d5d77b
gia <gia6shamugia@gmail.com>
2023-01-16 08:36:31 -0800

    adde swagger docs for /toDos endpoint

--- index.js
+++ index.js
@@ -2,9 +2,32 @@ const express = require("express");
 const { router } = require("./src/Router/router");
 const cors = require("cors");
 const { logger } = require("./src/Log/pino");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 require("./src/connection");
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "To-Do API",
+      version: "1.0.0",
+      description: "Simple express to-do-list API",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  apis: ["./src/Router/*js"],
+};
+
+const specs = swaggerJsDoc(options);
+
 const app = express();
+
+app.use("/swagger", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use(cors());
 app.use(express.json());
--- src/Router/router.js
+++ src/Router/router.js
@@ -10,6 +10,55 @@ const { validate } = require("../Validation/validate");
 const { validateId } = require("../Validation/validateId");
 const { body } = require("express-validator");
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     toDoItem:
+ *       type: object
+ *       required:
+ *         - toDoItem
+ *         - itemStatus
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the toDo item
+ *         toDoItem:
+ *           type: string
+ *           description: the content of the toDo item
+ *         itemStatus:
+ *           type: string
+ *           description: status of the toDo item
+ *       example:
+ *         id: 63c550acbc503a0f6c0d7b29
+ *         toDoItem: Getting up at 8am
+ *         itemStatus: active
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: To-do Items
+ *   description: The to-do items managing API
+ */
+
+/**
+ * @swagger
+ * /toDos:
+ *   get:
+ *     summary: Returns the list of all the to-do items
+ *     tags: [To-do Items]
+ *     responses:
+ *       200:
+ *         description: The list of the to-do items
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/toDoItem'
+ */
+
 router.get("/toDos", getToDosController);
 
 router.post(
