1d7f687770b965ce68ee383aac09fc187bbcbbd3
gia06 <gia6shamugia@gmail.com>
2023-01-16 05:44:25 -0800

    changed toDo schema

--- src/Schema/toDoSchema.js
+++ src/Schema/toDoSchema.js
@@ -5,7 +5,6 @@ const { Schema } = Mongoose;
 const toDoSchema = new Schema({
   toDoItem: String,
   itemStatus: String, // * active or completed
-  isDeleted: Boolean,
 });
 
 const toDoModel = Mongoose.model("toDoItem", toDoSchema);
